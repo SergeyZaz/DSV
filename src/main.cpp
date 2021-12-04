cc20cdd9b6d9b9028bdac62a79f4216f3d9e7d43
SergeyZaz <s.z.1980@yandex.ru>
2021-12-04 18:37:28 +0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,8 @@
 #include <QApplication>
 #include "zmainwindow.h"
-#include "windows.h"
 
 int main(int argc, char *argv[])
-{
-#ifdef _WIN32
-#ifdef _DEBUG
-				AllocConsole();
-				freopen("CONOUT$","wt", stdout);
-				freopen("CONOUT$","wt", stderr);
-				freopen("CONIN$","rt", stdin);
-				GetStdHandle(STD_OUTPUT_HANDLE);
-#endif
-#endif
-				
+{				
     QApplication app(argc, argv);
 
     ZMainWindow mainWin;
